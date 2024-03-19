5a32666818281cc4a34a802a7e4f3d662ceb61ca
A1danz <galeevaidan@yandex.ru>
2024-03-19 15:45:13 +0300

    add common-module

--- app/src/main/java/com/a1danz/posting/MainActivity.kt
+++ app/src/main/java/com/a1danz/posting/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.AttributeSet
 import android.view.View
-import com.a1danz.posting.databinding.SigninFragmentBinding
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "PostingApp"
 include(":app")
+include(":common")
