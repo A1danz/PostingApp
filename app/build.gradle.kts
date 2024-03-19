122ee972a4ef19426289ae63c5c9f1f7eec0b2ec
A1danz <galeevaidan@yandex.ru>
2024-03-19 16:34:44 +0300

    configure app-module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,31 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     implementation(platform(libs.firebase))
     implementation(libs.firebaseAnalytics)
+
+    implementation(libs.navigationUi)
+    implementation(libs.navigationFragment)
+
+    implementation(libs.dagger)
+    ksp(libs.daggerCompiler)
+
+    implementation(libs.lifecycle)
+    implementation(libs.coroutinesCore)
+    implementation(libs.coroutinesAndroid)
+
+    implementation(libs.roomKtx)
+    implementation(libs.roomRuntime)
+    ksp(libs.roomCompiler)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofitGsonConverter)
+    implementation(libs.gson)
+
+    implementation(libs.viewModelLifecycle)
+
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
