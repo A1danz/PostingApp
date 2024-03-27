125f3a5eb320e7e5a2406b3ddf4bba7dbd6bae26
A1danz <galeevaidan@yandex.ru>
2024-03-27 22:46:21 +0300

    sample logic to provides viewmodels

---
+++ app/src/main/java/com/a1danz/posting/di/presentation/DaggerViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.a1danz.posting.di.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+
+
+class DaggerViewModelFactory @Inject constructor(
+    private val viewModelMap: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>,
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val result = viewModelMap[modelClass] ?: throw IllegalStateException("ViewModel $modelClass not found")
+
+        return result.get() as T
+    }
+}
